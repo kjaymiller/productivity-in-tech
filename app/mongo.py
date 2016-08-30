7c1e8ac478c4fc8ef142a140d3c781d1e68b2646
Jay Miller <kjaymiller@gmail.com>
2016-08-29 21:45:13 -0700

    Delete mongo.py

--- app/mongo.py
+++
@@ -1,7 +0,0 @@
-import pymongo
-
-conn = pymongo.MongoClient('productivityintech.com', 27017)
-db = conn['pitpodcast']
-podcast_coll = db['podcasts']
-extended_coll = db['extended']
-friends_coll = db['friends']

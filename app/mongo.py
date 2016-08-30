8702ac7295f5e1ad8e5abda893a0bcef04f55a19
Kevin Miller <kevinmiller@Kevins-MacBook-Pro.local>
2016-08-29 21:42:38 -0700

    added list to PIT Podcast on webpage

--- app/mongo.py
+++ app/mongo.py
@@ -1,6 +1,6 @@
 import pymongo
 
-conn = pymongo.MongoClient('localhost', 27017)
+conn = pymongo.MongoClient('productivityintech.com', 27017)
 db = conn['pitpodcast']
 podcast_coll = db['podcasts']
 extended_coll = db['extended']

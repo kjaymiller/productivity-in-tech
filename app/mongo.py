450f7fcd69ad35394fb3a6dc45572a91366cf140
Kevin Miller <kevinmiller@Kevins-MacBook-Pro.local>
2016-09-05 14:11:07 -0700

    added authentication to mmongo

--- app/mongo.py
+++ app/mongo.py
@@ -1,9 +1,9 @@
 import pymongo
-from app.config_test import host, port, database  # use in test environment
-# from app.config import host, port, database
+from app.db_config import host, port, database, username, password
 
 conn = pymongo.MongoClient(host, port)
 db = conn[database]
+db.authenticate(username, password, source=database)
 podcast_coll = db['podcasts']
 extended_coll = db['extended']
 friends_coll = db['friends']

4d906ef4f16cf52c6dd90588945d317f9706c7fa
Jay Miller <kjaymiller@gmail.com>
2016-09-29 16:39:07 -0700

    Update config_template.py

--- app/config_template.py
+++ app/config_template.py
@@ -1,8 +1,18 @@
-# Replace variables with correct information 
-# database parameters
+"""
+Configuration template for your flask setup
+- Replace variables with correct information 
+-  database parameters
+-  remove any unused values
+"""
+
+# DATABASE INFORMATION
 host = [HOSTNAME]
 port = [PORT]
 database = [DATABASE_NAME]
 test_database = [TEST_DATABASE_NAME]
 username = [USERNAME]
 password = [PASSWORD]
+
+#DATABASE COLLECTIONS
+podcast_collection = [PODCAST_COLLECTION]
+blog_collection = [BLOG_POST_COLLECTION]

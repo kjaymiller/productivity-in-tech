744c3b198190497656749836ec7fba3a399eb259
Kevin Miller <kevinmiller@Kevins-MacBook-Pro.local>
2016-10-07 19:54:03 -0700

    removed files

--- add_podcast_photo.py
+++
@@ -1,8 +0,0 @@
-from sys import argv
-from app.mongo import podcast_coll
-
-photo_url = argv[1]
-episode_number = int(input('Enter the Episode Number: '))
-podcast_coll.find_one_and_update({'episode_number': episode_number},
-                                 {'$set': {'photo': photo_url}},
-                                 upsert=True)

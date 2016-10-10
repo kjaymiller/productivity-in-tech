77c388cb585804ee935c236c69890c9611ddc64f
Kevin Miller <kevinmiller@Kevins-MacBook-Pro.local>
2016-10-10 00:33:10 -0700

    add pitresources

---
+++ resources/links.py
@@ -0,0 +1,13 @@
+reflections = {
+            'itunes': 'https://itunes.apple.com/us/podcast/productivity-in-tech-reflections/id1161292423tunes',
+            'google': 'https://play.google.com/music/listen#/ps/I5essfo5jx2xknxq4vbrmylpjl4',
+            'tunein': 'http://tunein.com/radio/PIT-Reflections-p918568/',
+            'stitcher': 'http://www.stitcher.com/s?fid=101817&refid=stpr',
+            }
+            
+podcast = {
+        'itunes': 'https://itunes.apple.com/us/podcast/productivity-in-tech-podcast/id1086437786?mt=2',
+        'google': 'https://play.google.com/music/listen#/ps/Isoopwbe6zdbev5ijenegkcpp44',
+        'stitcher': 'http://app.stitcher.com/browse/feed/85598/details',
+        'tunein': 'http://tunein.com/radio/Productivity-in-Tech-Podcast-p894677/',
+}

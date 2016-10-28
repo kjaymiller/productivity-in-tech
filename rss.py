767efddde25a4c745a170700e3741d7fb7cd4a94
Kevin Miller <kevinmiller@Kevins-MacBook-Pro.local>
2016-10-27 21:21:28 -0700

    rss.py

--- rss.py
+++ rss.py
@@ -1,17 +1,22 @@
 import re
 from feedparser import parse
-from app.mongo import podcast_coll, pitreflections_coll
-
-feeds = ((podcast_coll, 'http://productivityintech.libsyn.com'),
-        (pitreflections_coll, 'http://pitreflections.libsyn.com'))
+from app.podcasts import episode_number_from_title, Podcast
 
 def parse_feed(collection, feed):
-    feed = parse('feed')
+    feed = parse(feed)
+    collection.drop()
     for entry in feed['entries']:
-        title = strip_episode_number(feed['title'])
-        print(title)
+        episode_number = episode_number_from_title(entry['title'])
+        title = strip_episode_from_title(entry['title'])
+        description = entry['description']
+        subtitle = entry['subtitle']
+        duration = entry['itunes_duration']
+        media_url = entry['link']
+        episode = Podcast(title=title, description=description, subtitle=subtitle,
+                        media_url=media_url, duration=duration, collection=collection,
+                        episode_number=episode_number)
+        collection.insert_one(episode.__dict__)
 
 
-def strip_episode_number(title):
-    episode_number = re.search(r'\D+(\d+)', title).group(1)
-    return episode_number
+def strip_episode_from_title(title):
+    return re.search('\D+\d+(.+)', title).group(1)

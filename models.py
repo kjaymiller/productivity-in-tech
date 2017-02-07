bc66611ac6964ea6031c8327fa11c4a7cfb6b15d
Kevin Miller <kevinmiller@Kevins-MacBook-Pro.local>
2017-02-07 06:22:17 -0800

    removed from file methods from models.py

--- models.py
+++ models.py
@@ -28,7 +28,7 @@ def generate_rss_feed(collection, atom=False):
         return'{channel}{items}</feed>'.format(channel=collection.rss, items=items)
 
     else:
-        items_list = collection.collection.find(sort=[('episode_number', DES)])
+        items_list = collection.collection.find({'published': True}, sort=[('episode_number', DES)])
         for item in items_list:
             if 'rss' in item.keys():
                 items += item['rss']
@@ -116,26 +116,16 @@ class Podcast(Collection):
 
 
 class Entry():
-    re_chk = ': *(.*)$'
-
-    def __init__(self, collection, from_file=None, title='', subtitle='', tags=[],
+    def __init__(self, collection, title='', subtitle='', tags=[],
                     author='', summary='', content=None, publish_date=atom_now,
                     podcast=None, check=False, duration=None, explicit=None):
-        if from_file:
-            self.title = self.header('title', text=from_file)
-            self.subtitle = self.header('subtitle', text=from_file)
-            self.author = self.header('author', text=from_file)
-            self.summary = self.header('summary', text=from_file)
-            self.tags = self.header('tags', text=from_file, delimit=True)
-            self.content = self.strip_headers(from_file)
 
-        elif all(title, content):
-            self.title = title
-            self.subtitle = subtitle
-            self.tags = tags
-            self.content = content
-            self.author = author
-            self.summary = summary
+        self.title = title
+        self.subtitle = subtitle
+        self.tags = tags
+        self.content = content
+        self.author = author
+        self.summary = summary
 
         self.publish_date = publish_date
         self.collection = c = collection
@@ -149,6 +139,7 @@ class Entry():
                                           'publish_date': self.publish_date,
                                           'feed': ''}).inserted_id
         url = '{}/{}/{}'.format(c.url, c_name, self.id)
+
         self.feed = """<entry><title>{title}</title>
 <link href="{url}" />
 <id>{url}</id>
@@ -160,28 +151,6 @@ class Entry():
             publish_date= self.publish_date)
         c.collection.find_one_and_update({'_id':self.id},{'$set':{'feed':self.feed}})
 
-
-
-    def header(self, val, text, delimit=False):
-        r = re.search('^' + val + self.re_chk, text, re.I|re.M)
-        if r:
-            print(r.group(0) + ' found!')
-            result = r.group(1)
-        else:
-            return ''
-
-        if delimit:
-            result = result.replace(';',',').split(',')
-        return result
-
-    def strip_headers(self, text):
-        index = 0
-
-        while re.match(r'\w+: *(.*)', text.splitlines()[index]):
-            index += 1
-
-        content_lines = text.splitlines()[index:]
-        return '\n'.join(content_lines)
 def latest_post(collection):
     return collection.collection.find_one(sort=[('publish_date', DES)])
 
@@ -199,44 +168,37 @@ def latest_episode(collections):
 
 class Episode(Entry):
     """Podcast Episode to be added  object that will be used to add information to the database."""
-    def __init__(self, collection, image_href='', media_url=None, episode_number=None,
-                from_file=None, title='', author='', subtitle='', tags=[],
-                summary='', content=None, publish_date=episode_now,
-                explicit='no', duration=None):
+    def __init__(self, title, collection, content, media_url, duration, episode_number=None,
+                subtitle='', tags=[], author='', summary='', publish_date=episode_now,
+                explicit='no'):
 
-        super().__init__(collection=collection, from_file=from_file,
+        super().__init__(collection=collection,
                 title=title, subtitle=subtitle, tags=tags, author=author,
                 summary=summary, content=content, publish_date=publish_date)
+
+        self.explicit = explicit
+        if not episode_number:
+            self.episode_number = last()
+        else:
+            self.episode_number = episode_number
+        self.media_url= media_url
 
         pub_datetime = datetime.strptime(publish_date, "%a, %d %b %Y %H:%M:%S %z")
         now = datetime.now(pytz.utc)
 
         if pub_datetime < now:
             published = True
+            print(title + ' published')
         else:
             published = False
-
-        if from_file:
-            self.episode_number = int(self.header('episode_number', text=from_file))
-            self.description = self.strip_headers(text=from_file)
-            self.media_url = self.header('media_url', text=from_file)
-            self.duration = self.header('duration', text=from_file)
-
-        else:
-            self.duration = duration
-            self.explicit = explicit
-            self.episode_number = last()
-            self.media_url= media_url
+            print(title + ' not published until ' + publish_date)
 
         c = collection
-        if image_href:
-            self.image_href = image_href
-        else:
-            self.image_href = c.logo_href
-
         url = '{}/{}/{}'.format(c.url, c.collection_name, self.episode_number)
         rss_title = '{} {}: {}'.format(c.abbreviation, self.episode_number, self.title)
-        length = len(urlopen(self.media_url).read())
+        length = len(urlopen(media_url).read())
+
+        image_href = c.logo_href
 
         # `duration` and `explicit` are not required by itunes
         # rss but should be included when necessary
@@ -249,7 +211,7 @@ class Episode(Entry):
             explicit = "<itunes:explicit>{}</itunes:explicit>".format(explicit)
         else:
             explicit = ''
-        self.rss = """<item><title>{rss_title}</title>
+        rss = """<item><title>{rss_title}</title>
 <pubDate>{publish_date}</pubDate>
 <guid><![CDATA[{url}]]></guid>
 <link><![CDATA[{url}]]></link>
@@ -261,16 +223,16 @@ class Episode(Entry):
 <itunes:keywords>{keywords}</itunes:keywords>
 <itunes:subtitle><![CDATA[{subtitle}]]></itunes:subtitle>
 </item>""".format(rss_title=rss_title, publish_date=publish_date,
-            media_url=self.media_url,length=length, duration=self.duration,
+            media_url=media_url,length=length, duration=duration,
             explicit=explicit, subtitle=subtitle, url=url,
-            description=markdown(self.description), image_href=self.image_href,
-            keywords=(' ').join(self.tags))
+            description=markdown(content), image_href=image_href,
+            keywords=(' ').join(tags))
 
-        c.collection.find_one_and_update({'_id':self.id},
-                {'$set':{'episode_number':self.episode_number,
-                'description': self.description, 'media_url':self.media_url,
-                'duration': duration, 'image_href':self.image_href,
-                'rss':self.rss, 'publish_date':publish_date, 'published':published}})
+        c.collection.find_one_and_update({'_id': self.id},
+                {'$set':{'episode_number': episode_number,
+                'description':content, 'media_url': media_url,
+                'duration': duration, 'image_href': image_href,
+                'rss': rss, 'publish_date': publish_date, 'published': published}})
 
 
 def total_pages(collection, page=None):
@@ -289,7 +251,6 @@ def total_pages(collection, page=None):
         'total': pages,
         'current': page
     }
-
 
     return nav
 

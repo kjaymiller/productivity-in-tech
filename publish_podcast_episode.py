bc109fcb57f5e033bac3fca98aed3b70d045ce06
Kevin Miller <kevinmiller@Kevins-MacBook-Pro.local>
2017-02-07 06:25:03 -0800

    create publish podcast

--- publish_podcast_episode.py
+++ publish_podcast_episode.py
@@ -5,10 +5,36 @@ from datetime import datetime
 from models import Episode, generate_rss_feed
 from paramiko import SSHClient, AutoAddPolicy
 from scp import SCPClient
+import re
 import pytz
 
-
 podcast = argv[1]
+
+def header(val, text, delimit=False):
+    re_chk = ': *(.*)$'
+    r = re.search('^' + val + re_chk, text, re.I|re.M)
+
+    if r:
+        print(r.group(0) + ' found!')
+        result = r.group(1)
+
+        if delimit:
+            return result.split(',')
+
+        else:
+            return result
+
+    else:
+        return ''
+
+def strip_headers(text):
+    index = 0
+
+    while re.match(r'\w+: *(.*)', text.splitlines()[index]):
+        index += 1
+
+    content_lines = text.splitlines()[index:]
+    return '\n'.join(content_lines)
 
 podcast_name = podcast.split('_')[0]
 episode_number = podcast.split('_')[1]
@@ -21,22 +47,48 @@ md_path = PARENT_PATH + '/{}.md'.format(podcast)
 ssh = SSHClient()
 ssh.set_missing_host_key_policy(AutoAddPolicy())
 ssh.connect('productivityintech.com', username='pituser')
-#with SCPClient(ssh.get_transport()) as scp:
-#    scp.put(mp3_path, '/mnt/volume-sfo2-01/files/podcast/')
+
+with SCPClient(ssh.get_transport()) as scp:
+    scp.put(mp3_path, '/mnt/volume-sfo2-01/files/podcast/')
+
 
 with open(md_path) as f:
-    if len(argv) > 2:
-        fmt = '%m%d%y %H%M'
-        unformatted_date = argv[2]
-        d = datetime.strptime(unformatted_date, fmt)
-        pub_date = d.replace(tzinfo=pytz.utc).strftime('%a, %d %b %Y %H:%M:%S %z')
+    from_file = f.read()
 
-        Episode(from_file=f.read(), collection=collection,
-                publish_date=pub_date)
-    else:
-        Episode(from_file=f.read(), collection=collection)
+if len(argv) > 2:
+    unformatted_date=(argv[2])
 
-    rss = generate_rss_feed(collection)
+else:
+    unformatted_date = header('publish_date', text=from_file)
+
+fmt = '%m%d%y %H%M'
+d = datetime.strptime(unformatted_date, fmt)
+publish_date = d.replace(tzinfo=pytz.utc).strftime('%a, %d %b %Y %H:%M:%S %z')
+
+title = header('title', text=from_file)
+subtitle = header('subtitle', text=from_file)
+author = header('author', text=from_file)
+summary = header('summary', text=from_file)
+tags = header('tags', text=from_file, delimit=True)
+episode_number = int(header('episode_number', text=from_file))
+media_url = header('media_url', text=from_file)
+duration = header('duration', text=from_file)
+content = strip_headers(from_file)
+
+
+Episode(collection=collection,
+        title=title,
+        subtitle=subtitle,
+        author=author,
+        tags=tags,
+        duration=duration,
+        summary=summary,
+        publish_date=publish_date,
+        episode_number=episode_number,
+        media_url=media_url,
+        content=content)
+
+rss = generate_rss_feed(collection)
 
 with open(rss_path, 'w') as f:
     f.write(rss)

30a2c36a27b6f8b76140326108bd8e4f7f510325
Kevin Miller <kevinmiller@Kevins-MacBook-Pro.local>
2017-07-31 07:07:26 -0700

    fix slack goals

--- slack.py
+++ slack.py
@@ -7,12 +7,12 @@ target_goal_day = 0
 class Goal():
     collection = db['slack_goals']
 
-def add_goal(self, user_id, goal):
-    data = {'goal': goal,
-            'goal_date': self.goal_date,
-            'user_id': user_id}
-    self.collection.insert_one(data)
-    return 'Goal Successfully Added: _{}_'.format(goal)
+    def add_goal(self, user_id, goal):
+        data = {'goal': goal,
+                'goal_date': self.goal_date,
+                'user_id': user_id}
+        self.collection.insert_one(data)
+        return 'Goal Successfully Added: _{}_'.format(goal)
 
     def retrieve_goal(self, user_id):
         goal = self.collection.find_one({'user_id':user_id}, sort=[('goal_date', -1)])

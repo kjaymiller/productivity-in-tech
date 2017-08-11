d561f4e33bcab3f995708828b90897541fe5dc62
Kevin Miller <kevinmiller@Kevins-MacBook-Pro.local>
2017-08-11 12:57:43 -0700

    added slack/goals/button

--- app/views.py
+++ app/views.py
@@ -233,6 +233,7 @@ def slack_goals():
 @app.route('/api/slack/goal/button', methods=['POST'])
 def slack_goal_buttons():
     content = request.json
+    print(content)
     user = content['user']['id']
     goal = Goal(user)
     if content['actions'][0]['value']=='complete':

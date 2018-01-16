8350f0061bf46dbab67c707e06fbc22fc4e8b8a3
Jay Miller <kjaymiller@admins-iMac.local>
2018-01-16 07:03:54 -0800

    made similar posts module to load for blog and podcast

---
+++ similar_posts.py
@@ -0,0 +1,9 @@
+def similar_posts(entry, collection):
+        posts = []
+        for tag in entry.get('tags', []):
+            entries = collection.find({'tags': tag})
+            posts.extend([('./'+ str(x['_id']), x['title']) for x in entries])
+
+        strip_post = filter(lambda x: x[1] != entry['title'], posts)
+        return Counter(strip_post).most_common(4)
+
